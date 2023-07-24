edabb2a44dbb125ed809d0ea0d40bdaa19940a4b
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2023-07-24 02:16:10 +0200

    feat: component - countdown

---
+++ src/components/countdown/Countdown.tsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+import styles from "./Countdown.module.css";
+function Countdown(props: {
+  secondsBeforeExpiration: number;
+  onExpire: () => void;
+  isOn: boolean;
+}) {
+  const [seconds, setSeconds] = useState(props.secondsBeforeExpiration || 3);
+
+  useEffect(() => {
+    if (!props.isOn) {
+      return;
+    }
+    if (seconds > 0) {
+      let timeoutId = setTimeout(
+        () => setSeconds((prevState) => prevState - 1),
+        1000
+      );
+      return () => clearTimeout(timeoutId);
+    } else {
+      props.onExpire();
+    }
+  }, [seconds, setSeconds, props.isOn]);
+
+  return <div className={styles.countDown}>{seconds}</div>;
+}
+
+export default Countdown;
