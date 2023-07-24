710d4c7c204d1fd8075cd0ba89ec6bc105c8e597
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2023-07-24 02:18:47 +0200

    feat: component - points

---
+++ src/components/points/Points.tsx
@@ -0,0 +1,12 @@
+import { useAppSelector } from "@store";
+import styles from "./Points.module.css";
+function Points() {
+  const points = useAppSelector((state) => state.quizReducer.totalPoints);
+  return (
+    <div data-testid="pointsTestAttribute" className={styles.points}>
+      Points: {points}
+    </div>
+  );
+}
+
+export default Points;
