e9ae9d309907d808326ea2bd3908fe466e64a615
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2023-07-24 02:15:39 +0200

    feat: component - back button

--- src/components/backButton/BackButton.tsx
+++ src/components/backButton/BackButton.tsx
@@ -1,12 +1,10 @@
 import Link from "next/link";
-import React from "react";
+import styles from "./BackButton.module.css";
 
 function BackButton() {
   return (
-    <Link
-      href="/"
-      className="self-center bg-gray-500 hover:bg-yellow-700 text-black font-bold py-2 px-4 rounded">
-      BackButton
+    <Link href="/" className={styles.backButton}>
+      Go back
     </Link>
   );
 }
