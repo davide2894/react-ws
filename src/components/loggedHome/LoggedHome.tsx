26ccb5dd71dd39dab335c90e5e52f7bab8aae619
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2023-07-24 02:17:16 +0200

    feat: component - home as logged user

--- src/components/loggedHome/LoggedHome.tsx
+++ src/components/loggedHome/LoggedHome.tsx
@@ -1,33 +1,30 @@
 import LogoutButton from "@components/logoutButton/LogoutButton";
 import { useAppSelector } from "@store";
 import Link from "next/link";
-import React from "react";
+import styles from "./LoggedHome.module.css";
 
 function LoggedHome() {
   const user = useAppSelector((state) => state.userReducer);
 
   return (
-    <>
-      <LogoutButton />
-      <div>LoggedHome</div>
-      <div>{user.isLogged ? "logged" : "not logged"}</div>
-      <div>{user.name}</div>
-      <Link
-        className="bg-gray-300 hover:bg-green-700 text-black font-bold py-2 px-4 rounded"
-        href="/leaderboard">
-        Leaderboard
-      </Link>
-      <Link
-        className="bg-gray-300 hover:bg-green-700 text-black font-bold py-2 px-4 rounded"
-        href="/user">
-        Check your own best scores
-      </Link>
-      <Link
-        className="bg-gray-300 hover:bg-green-700 text-black font-bold py-2 px-4 rounded"
-        href="/quiz">
-        Play Game!
-      </Link>
-    </>
+    <div className={styles.loggedHomeContainer}>
+      <div className={styles.welcome}>
+        Wecome
+        <span className={styles.userName}>{user.name}</span>
+      </div>
+      <div className={styles.actions}>
+        <LogoutButton shouldDisplayGameLossWarning={false} />
+        <Link className={styles.link} href="/highscore">
+          Global high scores
+        </Link>
+        <Link className={styles.link} href="/user">
+          Personal scores
+        </Link>
+        <Link className={styles.link} href="/quiz">
+          Play
+        </Link>
+      </div>
+    </div>
   );
 }
 
