15c4a714f149e5159e34085ab4756859e8cc09b2
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2023-07-24 02:26:44 +0200

    docs(utils): add descriptive comments to log util function

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,3 +1,10 @@
+/**
+ * @description show console logs only in dev mode
+ * @param args
+ * @returns
+ *  - empty function if we are in production
+ *  - otherwise it returns undefined (default return value for any js function)
+ */
 export default function log(...args: any[]) {
   if (process.env.NODE_ENV === "production") {
     return () => {};
