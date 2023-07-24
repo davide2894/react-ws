4384f8b643ac46497886ac8226a5b66f4df1d868
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2023-07-24 02:25:04 +0200

    feat: add types

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,60 @@
+import { type } from "os";
+
 export interface UserTypeInLocalDb {
   name: string;
-  personalGames: Array<PersonalGame>;
+  personalScores: Array<Score>;
 }
 
 export interface User extends UserTypeInLocalDb {
   isLogged: boolean;
 }
+export interface Score {
+  dateString: string;
+  time: number;
+  points: number;
+}
 
-export interface PersonalGame {}
+export interface HighScore extends Score {
+  name: string;
+  runs: number;
+}
+
+export interface QuizType {
+  initialized: boolean;
+  questions: Array<QuestionType>;
+  totalPoints: number;
+  numberOfQuestionsAnswered: number;
+  numberOfCorrectAnswers: number;
+  dateString: string;
+  startDateInMilliseconds: number;
+  endDateInMilliseconds: number;
+  totalTime: number | string;
+}
+
+export interface QuestionType {
+  trackId: number;
+  trackName: string;
+  trackSnippet: {
+    id: number;
+    body: string;
+  };
+  choices: [
+    {
+      id: number;
+      name: string;
+      isCorrectChoice: boolean;
+    },
+    {
+      id: number;
+      name: string;
+      isCorrectChoice: boolean;
+    },
+    {
+      id: number;
+      name: string;
+      isCorrectChoice: boolean;
+    }
+  ];
+  isAnswered: boolean;
+  isAnsweredCorrectly: boolean;
+}
