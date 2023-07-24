dbdb787adf10507e6646dabe31ad19292b7dc5a0
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2023-07-24 02:23:42 +0200

    feat: page - add user page

--- src/pages/user.tsx
+++ src/pages/user.tsx
@@ -1,12 +1,48 @@
 import BackButton from "@components/backButton/BackButton";
-import React from "react";
+import UserScoreTable from "@components/userScoreTable/UserScoreTable";
+import { useAppSelector } from "@store";
+import log from "@utils/log";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React, { useEffect } from "react";
+import styles from "./user.module.css";
 
 function user() {
+  const user = useAppSelector((state) => state.userReducer);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!user.isLogged) {
+      router.push("/");
+    }
+  }, [router, user.isLogged]);
+
+  let content;
+
+  if (
+    user &&
+    user.isLogged &&
+    user.personalScores &&
+    user.personalScores.length
+  ) {
+    content = <UserScoreTable user={user} />;
+  } else {
+    content = (
+      <div className={styles.personalScoresEmpty}>
+        <p>
+          "You haven't played any game yet. Play one to see your scores here"
+        </p>
+        <Link href="quiz">Play a game!</Link>
+      </div>
+    );
+  }
+
   return (
-    <>
+    <div className="scoresPage personalScoresPage">
       <BackButton />
-      <div>user</div>
-    </>
+      <h2>Personal scores of the last games</h2>
+      {content}
+    </div>
   );
 }
 
