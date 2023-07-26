ea13046b37817a729fe34df8dcfde404f34456e4
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2023-07-26 13:20:18 +0200

    refactor(logout button): update redux file paths

--- src/components/loggedHome/LoggedHome.tsx
+++ src/components/loggedHome/LoggedHome.tsx
@@ -1,5 +1,5 @@
 import LogoutButton from "@components/logoutButton/LogoutButton";
-import { useAppSelector } from "@store";
+import { useAppSelector } from "src/store/store";
 import Link from "next/link";
 import styles from "./LoggedHome.module.css";
 
