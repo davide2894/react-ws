5d505be8d3568c87be1a267f341b9db1f708e27c
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2023-07-26 13:26:24 +0200

    refactor(LoggedOutHome): view structure

--- src/components/loggedOutHome/LoggedOutHome.tsx
+++ src/components/loggedOutHome/LoggedOutHome.tsx
@@ -4,24 +4,22 @@ import styles from "./LoggedOut.module.css";
 
 function LoggedOutHome() {
   return (
-    <>
-      <div className={styles.container}>
-        <div className={styles.intro}>
-          <p>Welcome dear visitor, you find yourself in a music quiz game.</p>
-          <p>Let's see how good you can do!</p>
-          <p>
-            Log in and play a game or check the high scores so far to get an
-            idea of who your competing with!
-          </p>
-        </div>
-        <div className={styles.actions}>
-          <LoginButton />
-          <Link className={styles.link} href="/highscore">
-            High Score
-          </Link>
-        </div>
+    <div className={styles.container}>
+      <div className={styles.intro}>
+        <p>Welcome dear visitor, you find yourself in a music quiz game.</p>
+        <p>Try your best!</p>
+        <p>
+          Log in and play a game or check the high scores so far to get an idea
+          of who your competing with!
+        </p>
       </div>
-    </>
+      <div className={styles.actions}>
+        <LoginButton />
+        <Link className={styles.link} href="/highscore">
+          High Score
+        </Link>
+      </div>
+    </div>
   );
 }
 
