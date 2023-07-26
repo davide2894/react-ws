031076f7e3e059af82a721089f0330a7bf852bd2
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2023-07-26 13:20:59 +0200

    refactor(progress): improve readability for user

--- src/components/progress/Progress.tsx
+++ src/components/progress/Progress.tsx
@@ -1,14 +1,15 @@
-import { useAppSelector } from "@store";
+import { useAppSelector } from "src/store/store";
 import styles from "./Progress.module.css";
 
 function Progress() {
   const quizState = useAppSelector((state) => state.quizReducer);
   return (
     <div className="progress">
+      <span>You are on question </span>
       <span className={styles.numberOfQuestionsAnswered}>
         {quizState.numberOfQuestionsAnswered + 1}
       </span>
-      <span> / </span>
+      <span> out of </span>
       <span className={styles.totalQuestions}>
         {quizState.questions.length}
       </span>
