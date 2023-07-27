bb803663e8920f7d3c581fb081e8533a13cae8af
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2023-07-27 14:53:16 +0200

    style: use nextjs fonts functionality for improved fonts performance

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,10 +1,17 @@
 import Footer from "@components/footer/Footer";
 import TopBar from "@components/topBar/TopBar";
 import styles from "./Layout.module.css";
+import { Lato } from "next/font/google";
+const lato = Lato({
+  style: "normal",
+  weight: ["100", "400", "700", "900"],
+  subsets: ["latin"],
+  preload: true,
+});
 
 function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex flex-col min-h-screen layout">
+    <div className={lato.className}>
       <TopBar />
 
       <div className={styles.layoutBody}>{children}</div>
