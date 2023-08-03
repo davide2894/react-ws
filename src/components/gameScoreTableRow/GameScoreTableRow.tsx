34c5118a97a6a42b17cd675ef70b2c594434ce2e
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2023-08-03 18:35:24 +0200

    fix(scores table): do not show score id as table entry in row

--- src/components/gameScoreTableRow/GameScoreTableRow.tsx
+++ src/components/gameScoreTableRow/GameScoreTableRow.tsx
@@ -1,10 +1,14 @@
+import log from "@utils/log";
+
 function GameScoreTableRow(props: { score: any; position: number }) {
   const { score, position } = props;
   return (
     <tr>
       <td>{position}</td>
       {Object.keys(score).map((objKey, idx: number) => {
-        return <td key={`${idx}-${score[objKey]}`}>{score[objKey]}</td>;
+        if (objKey !== "id") {
+          return <td key={`${idx}-${score[objKey]}`}>{score[objKey]}</td>;
+        }
       })}
     </tr>
   );
