a7dc20c51e4e0bf04b0286bc8a10e6c27a5f4353
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2023-08-10 18:28:17 +0200

    docs(shuffleArray util): add comments to specify reason for choosing this algorithm

--- src/utils/shuffleArray.ts
+++ src/utils/shuffleArray.ts
@@ -1,6 +1,10 @@
-// using the Fisher-Yates algorith in combination with Math.Random()
+// using the Fisher-Yates algorithm in combination with Math.Random()
 // see: https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
 // see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+// note:
+//  - time complexity is O(n)
+//  - source: https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#The_modern_algorithm
+
 export function shuffleArray(arr: any[]) {
   for (let i = arr.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
