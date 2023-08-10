ff44dc7fa11c913efdc522a646b8037d6b12a864
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2023-08-10 18:26:56 +0200

    fix: back button text and href depending on if user is logged or not

--- src/components/backButton/BackButton.tsx
+++ src/components/backButton/BackButton.tsx
@@ -1,10 +1,22 @@
 import Link from "next/link";
 import styles from "./BackButton.module.css";
+import { useAppSelector } from "src/customHooks/useAppSelector";
 
 function BackButton() {
+  const user = useAppSelector((user) => user.userReducer);
+  let targetUrl;
+  let text;
+  if (user.isLogged) {
+    targetUrl = "/quiz/lobby";
+    text = "Back to game lobby";
+  } else {
+    targetUrl = "/";
+    text = "Back to homepage";
+  }
+
   return (
-    <Link href="/quiz/lobby" className={styles.backButton}>
-      Back to game lobby
+    <Link href={targetUrl} className={styles.backButton}>
+      {text}
     </Link>
   );
 }
